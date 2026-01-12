6513b7fa9af9069e4a421393f536dad2e1b4e4b4
Florian Holzmann <831-6191@gmx.de>
2026-01-12 22:05:13 +0100

    #mod: correct export syntax

--- libs/AhnalyticBase/Export.hpp
+++ libs/AhnalyticBase/Export.hpp
@@ -1,7 +1,11 @@
 #pragma warning(disable : 4251)
 
+#if defined(_WIN32) || defined(_WIN64)
 #ifdef AHNALYTICBASE_EXPORTS
 #define DLLEXPORT __declspec(dllexport)
 #else
 #define DLLEXPORT __declspec(dllimport)
+#endif
+#else
+#define DLLEXPORT __attribute__((visibility("default")))
 #endif
