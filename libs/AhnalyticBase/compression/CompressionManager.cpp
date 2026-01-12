6eac1adf24994fcac3f0415e6c605116f1b86835
Florian Holzmann <831-6191@gmx.de>
2026-01-12 22:24:18 +0100

    #mod. missing linux include for std::find

--- libs/AhnalyticBase/compression/CompressionManager.cpp
+++ libs/AhnalyticBase/compression/CompressionManager.cpp
@@ -14,6 +14,7 @@
 #include "AhnalyticBase/helper/Diagnostic.hpp"
 
 #include <cstring> 
+#include <algorithm>
 
 CompressionManager::CompressionManager()
 {
--- libs/AhnalyticBase/database/GitHubRepoDatabase.cpp
+++ libs/AhnalyticBase/database/GitHubRepoDatabase.cpp
@@ -7,6 +7,7 @@
 
 #include <nlohmann/json.hpp>
 #include <sstream>
+#include <algorithm>
 
 using json = nlohmann::json;
 
